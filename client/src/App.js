369878bd3421ea3a08b426fdcc168b56689a00f3
HoneyMS <Honeyms@mail.ru>
2021-02-26 18:10:19 +0300

    star is falling

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { GamePart } from './Component/GamePart';
+
+function App() {
+  return (
+    <div className="App">
+     <GamePart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Component/GamePart/index.js
@@ -0,0 +1,14 @@
+import s from './style.module.css'
+
+export function GamePart(params) {
+	
+	
+	return(
+<div className={s.border}>
+<div className={s.star}>
+<h1>5</h1>
+</div>
+
+</div>
+	)
+}
